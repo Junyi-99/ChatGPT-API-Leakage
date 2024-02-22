df1ff56f2419935d40bde54f5fef1629735351c2
Junyi Hou <hji200914@gmail.com>
2024-02-22 20:42:29 +0800

    reformat

--- main.py
+++ main.py
@@ -10,138 +10,165 @@ from selenium.common.exceptions import UnableToSetCookieException
 from selenium.webdriver.common.by import By
 from tqdm import tqdm
 
-from utils import (check_key, db_close, db_delete, db_get_all_keys, db_insert,
-                   db_key_exists, db_open, db_remove_duplication)
+from utils import (
+    check_key,
+    db_close,
+    db_delete,
+    db_get_all_keys,
+    db_insert,
+    db_key_exists,
+    db_open,
+    db_remove_duplication,
+)
 
 logging.basicConfig(level=logging.INFO)
-logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-
-DB_FILE = 'github.db'
-
-def login(driver: webdriver.Chrome):
-
-    cookie_exists = os.path.exists('cookies.pkl')
-    driver.get('https://github.com/login' if cookie_exists else 'https://github.com/')
-
-    if not cookie_exists:
-        logging.info("🤗 No cookies found, please login to GitHub first")
-        input("Press Enter after logged in: ")
-        with open("cookies.pkl", "wb") as file:
-            pickle.dump(driver.get_cookies(), file)
-            logging.info("🍪 Cookies saved")
-    else:
-        logging.info("🍪 Cookies found, loading cookies")
-        with open("cookies.pkl", "rb") as file:
-            cookies = pickle.load(file)
-
-        for cookie in cookies:
-            try:
-                driver.add_cookie(cookie)
-            except UnableToSetCookieException as e:
-                logging.debug(f"🟡 Warning, unable to set a cookie {cookie}")
-
-    driver.get('https://github.com/')
-    # TODO: check if the user is logged in, if cookies are expired, etc.
-
-def search(driver: webdriver.Chrome, url: str):
-    con, cur = db_open('github.db')
-
-    driver.get(url)
-    pattern = re.compile(r'sk-[a-zA-Z0-9]{48}')
-
-    while True:
-        driver.implicitly_wait(5)
-
-        # Expand all the code
-        [element.click() for element in driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'more match')]")]
+logging.basicConfig(format="%(asctime)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S")
 
 
-        codes = driver.find_elements(by=By.CLASS_NAME, value='code-list') # find all elements with class name 'f4'
-        for element in codes:
-            apis = pattern.findall(element.text)
-            if len(apis) == 0:
+class Leakage:
+    def __init__(self, db_file: str, keywords: list, languages: list):
+        self.db_file = db_file
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5)
+        self.con, self.cur = db_open(self.db_file)
+        
+        self.keywords = keywords
+        self.languages = languages
+        self.candidate = []
+        for keyword in self.keywords:
+            for language in self.languages:
+                self.candidate.append(
+                    f"https://github.com/search?q={keyword}+AND+%28%2Fsk-%5Ba-zA-Z0-9%5D%7B48%7D%2F%29+language%3A{language}&type=code&ref=advsearch"
+                )
+
+    def login(self):
+        cookie_exists = os.path.exists("cookies.pkl")
+        self.driver.get(
+            "https://github.com/login" if cookie_exists else "https://github.com/"
+        )
+
+        if not cookie_exists:
+            logging.info("🤗 No cookies found, please login to GitHub first")
+            input("Press Enter after you logged in: ")
+            with open("cookies.pkl", "wb") as file:
+                self.pickle.dump(self.driver.get_cookies(), file)
+                logging.info("🍪 Cookies saved")
+        else:
+            logging.info("🍪 Cookies found, loading cookies")
+            with open("cookies.pkl", "rb") as file:
+                cookies = pickle.load(file)
+
+            for cookie in cookies:
+                try:
+                    self.driver.add_cookie(cookie)
+                except UnableToSetCookieException as e:
+                    logging.debug(f"🟡 Warning, unable to set a cookie {cookie}")
+        
+        logging.info("🤗 Redirecting ...")
+        self.driver.get("https://github.com/")
+        # TODO: check if the user is logged in, if cookies are expired, etc.
+
+    def __search(self, url: str):
+        self.driver.get(url)
+        pattern = re.compile(r"sk-[a-zA-Z0-9]{48}")
+
+        while True:
+            # Expand all the code
+            [
+                element.click()
+                for element in self.driver.find_elements(
+                    by=By.XPATH, value="//*[contains(text(), 'more match')]"
+                )
+            ]
+
+            codes = self.driver.find_elements(
+                by=By.CLASS_NAME, value="code-list"
+            )  # find all elements with class name 'f4'
+            for element in codes:
+                apis = pattern.findall(element.text)
+                if len(apis) == 0:
+                    continue
+
+                apis = list(set(apis))
+                apis = [api for api in apis if not db_key_exists(self.cur, api)]
+
+                with ThreadPoolExecutor(max_workers=10) as executor:
+                    results = list(executor.map(check_key, apis))
+                    for idx, result in enumerate(results):
+                        db_insert(self.con, self.cur, apis[idx], result)
+
+            next_buttons = self.driver.find_elements(
+                by=By.XPATH, value="//a[@aria-label='Next Page']"
+            )
+            if len(next_buttons) == 0:
+                logging.debug("⚪️ No more pages")
+                break
+            next_buttons[0].click()
+
+    def search(self, from_iter: int = 0):
+        for idx, url in tqdm(enumerate(self.candidate), total=len(self.candidate)):
+            if idx < from_iter:
                 continue
+            self.__search(url)
+            logging.info(f"🔍 Finished {url}")
 
-            apis = list(set(apis))
-            apis = [api for api in apis if not db_key_exists(cur, api)]
+    def deduplication(self):
+        db_remove_duplication(self.con, self.cur)
 
-            with ThreadPoolExecutor(max_workers=10) as executor:
-                results = list(executor.map(check_key, apis))
-                for idx, result in enumerate(results):
-                    db_insert(con, cur, apis[idx], result)
+    def update_existed_keys(self):
+        keys = db_get_all_keys(self.cur)
+        for key in keys:
+            result = check_key(key[0])
+            db_delete(self.con, self.cur, key[0])
+            db_insert(self.con, self.cur, key[0], result)
 
-        next_buttons = driver.find_elements(by=By.XPATH, value="//a[@aria-label='Next Page']")
-        if len(next_buttons) == 0:
-            logging.debug("⚪️ No more pages")
-            break
-        next_buttons[0].click()
+    def __del__(self):
+        self.driver.quit()
+        self.con.commit()
+        db_close(self.con)
 
-    con.commit()
-    db_close(con)
 
 def main():
-    driver = webdriver.Chrome()
-    login(driver)
     keywords = [
-        'openai',
-        'gpt4',
-        'lab',
-        'experiment',
-        'gpt',
-        'chatgpt',
-        'gpt-3',
+        'rag',
         'llm',
         'agent',
-        'rag',
-        '实验',
-        '测试',
-        '语言模型',
+        "openai",
+        "gpt4",
+        "lab",
+        "experiment",
+        "gpt",
+        'key',
+        'apikey',
+        "chatgpt",
+        "gpt-3",
+        "llm",
+        "agent",
+        "rag",
+        "实验",
+        "测试",
+        "语言模型",
+        "密钥"
     ]
 
     languages = [
-        'Python',
-        'C',
-        'Java',
-        'JavaScript',
-        'Go',
-        'Shell',
-        'Rust',
-        'TypeScript',
-        'C++',
-        'C#',
-        'PHP',
-        'Ruby',
-        'Swift',
-        'Kotlin',
-        'Objective-C',
-        'Dart',
-        'Scala',
-        'Lua',
-        'Perl',
+        '"Jupyter Notebook"',
+        "Python",
+        "C++",
+        "Java",
+        "JavaScript",
+        "Go",
+        "Shell",
+        "Rust",
+        "TypeScript",
+        "PHP",
     ]
 
-    candidate = []
-    for keyword in keywords:
-        for language in languages:
-            candidate.append(f'https://github.com/search?q={keyword}+AND+%28%2Fsk-%5Ba-zA-Z0-9%5D%7B48%7D%2F%29+language%3A{language}&type=code&ref=advsearch')
+    leakage = Leakage("github.db", keywords, languages)
+    leakage.login()
+    leakage.search(from_iter=0)
+    leakage.update_existed_keys()
+    leakage.deduplication()
 
-    for url in tqdm(candidate):
-        search(driver, url)
-
-def update_existed_keys(con: Connection, cur: Cursor):
-    keys = db_get_all_keys(cur)
-    for key in keys:
-        result = check_key(key[0])
-        db_delete(con, cur, key[0])
-        db_insert(con, cur, key[0], result)
-
-
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
     main()
-    
-    con, cur = db_open(DB_FILE)
-    update_existed_keys(con, cur)
-    db_remove_duplication(con, cur)
-    con.commit()
-    db_close(con)
