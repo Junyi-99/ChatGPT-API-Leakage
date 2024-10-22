ffe34b6e9ef9e3aec404577bda9f1a879f52c338
Junyi Hou <hji200914@gmail.com>
2024-10-22 16:49:06 +0800

    chore: code structure refinement

---
+++ src/main.py
@@ -0,0 +1,275 @@
+"""
+Scan GitHub for available OpenAI API Keys
+"""
+import argparse
+import logging
+import os
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+import rich
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from tqdm import tqdm
+
+from configs import KEYWORDS, REGEX_LIST
+from manager import CookieManager, DatabaseManager, ProgressManager
+from utils import check_key
+
+FORMAT = "%(message)s"
+logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt="[%X]")
+log = logging.getLogger("ChatGPT-API-Leakage")
+httpx_logger = logging.getLogger("httpx")
+httpx_logger.setLevel(logging.WARNING)
+
+
+class APIKeyLeakageScanner:
+    """
+    Scan GitHub for available OpenAI API Keys
+    """
+    def __init__(self, db_file: str, keywords: list):
+        self.db_file = db_file
+        self.progress = ProgressManager()
+        self.driver: webdriver.Chrome | None = None
+        self.cookies: CookieManager | None = None
+        rich.print(f"📂 Opening database file {self.db_file}")
+
+        self.dbmgr = DatabaseManager(self.db_file)
+
+        self.keywords = keywords
+        self.candidate_urls = []
+        for regex, too_many_results, _ in REGEX_LIST:
+            for keyword in self.keywords:
+                if too_many_results: # if the regex is too many results, then we need to add AND condition
+                    self.candidate_urls.append(f"https://github.com/search?q={keyword}+AND+(/{regex.pattern}/)&type=code&ref=advsearch")
+                else: # if the regex is not too many results, then we just need the regex
+                    self.candidate_urls.append(f"https://github.com/search?q=(/{regex.pattern}/)&type=code&ref=advsearch")
+
+    def login_to_github(self):
+        """
+        Login to GitHub
+        """
+        rich.print("🌍 Opening Chrome ...")
+
+        options = webdriver.ChromeOptions()
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--ignore-ssl-errors")
+
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.implicitly_wait(3)
+
+        self.cookies = CookieManager(self.driver)
+
+        cookie_exists = os.path.exists("cookies.pkl")
+        self.driver.get("https://github.com/login")
+
+        if not cookie_exists:
+            rich.print("🤗 No cookies found, please login to GitHub first")
+            input("Press Enter after you logged in: ")
+            self.cookies.save()
+        else:
+            rich.print("🍪 Cookies found, loading cookies")
+            self.cookies.load()
+
+        self.cookies.verify_user_login()
+
+    def _process_url(self, url: str):
+        """
+        Process a search query url
+        """
+        if self.driver is None:
+            raise ValueError("Driver is not initialized")
+
+        self.driver.get(url)
+        apis_found = []
+        urls_need_expand = []
+
+        while True: # Loop until all the pages are processed
+            # If current webpage is reached the rate limit, then wait for 30 seconds
+            if self.driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'You have exceeded a secondary rate limit')]"):
+                for _ in tqdm(range(30), desc="⏳ Rate limit reached, waiting ..."):
+                    time.sleep(1)
+                self.driver.refresh()
+                continue
+
+            # Expand all the code
+            elements = self.driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'more match')]")
+            for element in elements:
+                element.click()
+
+            # find all elements with class name 'f4'
+            codes = self.driver.find_elements(by=By.CLASS_NAME, value="code-list")
+            for element in codes:
+                apis = []
+                # Check all regex for each code block
+                for regex, _, too_long in REGEX_LIST[2:]:
+                    if not too_long:
+                        apis.extend(regex.findall(element.text))
+
+                if len(apis) == 0:
+                    # Need to show full code. (because the api key is too long)
+                    # get the <a> tag
+                    a_tag = element.find_element(by=By.XPATH, value=".//a")
+                    urls_need_expand.append(a_tag.get_attribute("href"))
+                apis_found.extend(apis)
+
+            rich.print(f"🌕 There are {len(urls_need_expand)} urls waiting to be expanded")
+
+            try:
+                next_buttons = self.driver.find_elements(by=By.XPATH, value="//a[@aria-label='Next Page']")
+                rich.print("    🔍 Clicking next page")
+                WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, "//a[@aria-label='Next Page']")))
+                next_buttons = self.driver.find_elements(by=By.XPATH, value="//a[@aria-label='Next Page']")
+                next_buttons[0].click()
+            except Exception as _:
+                rich.print("    ⚪️ No more pages")
+                break
+
+
+        # Handle the expand_urls
+        for url in tqdm(urls_need_expand, desc="🔍 Expanding URLs ..."):
+            if self.driver is None:
+                raise ValueError("Driver is not initialized")
+
+            self.driver.get(url)
+            time.sleep(3) # TODO: find a better way to wait for the page to load
+
+            retry = 0
+            while retry <= 3:
+                matches = []
+                for regex, _, _ in REGEX_LIST:
+                    matches.extend(regex.findall(self.driver.page_source))
+                matches = list(set(matches))
+
+                if len(matches) == 0:
+                    rich.print(f"    ⚪️ No matches found in the expanded page, retrying [{retry}/3]...")
+                    retry += 1
+                    time.sleep(3)
+                    continue
+
+                with self.dbmgr as mgr:
+                    new_apis = [api for api in matches if not mgr.key_exists(api)]
+                    new_apis = list(set(new_apis))
+                apis_found.extend(new_apis)
+                rich.print(f"    🟢 Found {len(matches)} matches in the expanded page, adding them to the list")
+                break
+
+        self.check_api_keys_and_save(apis_found)
+
+    def check_api_keys_and_save(self, keys: list[str]):
+        """
+        Check a list of API keys
+        """
+        with self.dbmgr as mgr:
+            unique_apis = list(set(keys))
+            unique_apis = [api for api in unique_apis if not mgr.key_exists(api)]
+
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            results = list(executor.map(check_key, keys))
+            with self.dbmgr as mgr:
+                for idx, result in enumerate(results):
+                    mgr.insert(unique_apis[idx], result)
+
+    def search(self, from_iter: int | None = None):
+        """
+        Search for API keys, and save the results to the database
+        """
+        total = len(self.candidate_urls)
+        pbar = tqdm(
+            enumerate(self.candidate_urls),
+            total=total,
+            desc="🔍 Searching ...",
+        )
+
+        if from_iter is None:
+            from_iter = self.progress.load(total=total)
+
+        for idx, url in enumerate(self.candidate_urls):
+            if idx < from_iter:
+                pbar.update()
+                time.sleep(0.05)  # let tqdm print the bar
+                log.debug("⚪️ Skip %s", url)
+                continue
+            self._process_url(url)
+            self.progress.save(idx, total)
+            log.debug("🔍 Finished %s", url)
+            pbar.update()
+        pbar.close()
+
+    def deduplication(self):
+        with self.dbmgr as mgr:
+            mgr.deduplicate()
+
+    def update_existed_keys(self):
+        with self.dbmgr as mgr:
+            rich.print("🔄 Updating existed keys")
+            keys = mgr.all_keys()
+            for key in tqdm(keys, desc="🔄 Updating existed keys ..."):
+                result = check_key(key[0])
+                mgr.delete(key[0])
+                mgr.insert(key[0], result)
+
+    def all_available_keys(self) -> list:
+        with self.dbmgr as mgr:
+            return mgr.all_keys()
+
+    def __del__(self):
+        if hasattr(self, "driver"):
+            self.driver.quit()
+
+
+
+def main(from_iter: int | None = None, check_existed_keys_only: bool = False, keywords: list | None = None):
+    if keywords is None:
+        keywords = KEYWORDS.copy()
+    leakage = APIKeyLeakageScanner("github.db", keywords)
+
+    if not check_existed_keys_only:
+        leakage.login_to_github()
+        leakage.search(from_iter=from_iter)
+
+    leakage.update_existed_keys()
+    leakage.deduplication()
+    keys = leakage.all_available_keys()
+
+    rich.print(f"🔑 [bold green]Available keys ({len(keys)}):[/bold green]")
+    for key in keys:
+        rich.print(f"'{key}'")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--from-iter", type=int, default=None,
+                        help="Start from the specific iteration")
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        default=False,
+        help="Enable debug mode, otherwise INFO mode. Default is False (INFO mode)",
+    )
+    parser.add_argument(
+        "-ceko",
+        "--check-existed-keys-only",
+        action="store_true",
+        default=False,
+        help="Only check existed keys",
+    )
+    parser.add_argument(
+        "-k",
+        "--keywords",
+        nargs="+",
+        default=KEYWORDS,
+        help="Keywords to search",
+    )
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    main(
+        from_iter=args.from_iter,
+        check_existed_keys_only=args.check_existed_keys_only,
+        keywords=args.keywords
+    )
---
+++ src/manager.py
@@ -0,0 +1,164 @@
+"""
+This module is used to manage the progress and the cookies.
+
+It includes the following classes:
+- ProgressManager: to manage the progress
+- CookieManager: to manage the cookies
+- DatabaseManager: to manage the database
+"""
+import logging
+import os
+import sys
+import pickle
+import sqlite3
+import time
+from datetime import date
+from sqlite3 import Connection, Cursor
+
+from selenium.common.exceptions import UnableToSetCookieException
+from selenium.webdriver.common.by import By
+
+FORMAT = "%(message)s"
+logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt="[%X]")
+log = logging.getLogger("ChatGPT-API-Leakage")
+
+
+class ProgressManager:
+    def __init__(self, progress_file=".progress.txt"):
+        self.progress_file = progress_file
+
+    def save(self, from_iter: int, total: int):
+        with open(self.progress_file, "w") as file:
+            file.write(f"{from_iter}/{total}/{time.time()}")
+
+    def load(self, total: int) -> int:
+        if not os.path.exists(self.progress_file):
+            return 0
+
+        with open(self.progress_file, "r") as file:
+            last_, totl_, tmst_ = file.read().strip().split("/")
+            last, totl = int(last_), int(totl_)
+
+        if time.time() - float(tmst_) < 3600 and totl == total:
+            action = input(f"🔍 Progress found, do you want to continue from the last progress ({
+                           last}/{totl})? [yes] | no: ").lower()
+            if action in {"yes", "y", ""}:
+                return last
+
+        return 0
+
+
+class CookieManager:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def save(self):
+        cookies = self.driver.get_cookies()
+        with open("cookies.pkl", "wb") as file:
+            pickle.dump(cookies, file)
+            log.info("🍪 Cookies saved")
+
+    def load(self):
+        try:
+            with open("cookies.pkl", "rb") as file:
+                cookies = pickle.load(file)
+                for cookie in cookies:
+                    try:
+                        self.driver.add_cookie(cookie)
+                    except UnableToSetCookieException:
+                        log.debug("🟡 Warning, unable to set a cookie %s", cookie)
+        except (EOFError, pickle.UnpicklingError):
+            if os.path.exists("cookies.pkl"):
+                os.remove("cookies.pkl")
+            log.error("🔴 Error, unable to load cookies, invalid cookies has been removed, please restart.")
+
+    def verify_user_login(self):
+        """
+        Test if the user is really logged in
+        """
+        log.info("🤗 Redirecting ...")
+        self.driver.get("https://github.com/")
+
+        if self.driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'Sign in')]"):
+            if os.path.exists("cookies.pkl"):
+                os.remove("cookies.pkl")
+            log.error(
+                "🔴 Error, you are not logged in, please restart and try again.")
+            sys.exit(1)
+        return True
+
+
+class DatabaseManager:
+    """
+    This class is used to manage the database.
+    """
+    def __init__(self, db_filename: str):
+        self.db_filename = db_filename
+        self.con: Connection | None = None
+        self.cur: Cursor | None = None
+
+    def __enter__(self):
+        if not os.path.exists(self.db_filename):
+            logging.info("Creating database github.db")
+
+        self.con = sqlite3.connect(self.db_filename)
+        self.cur = self.con.cursor()
+
+        self.cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='APIKeys'")
+        if self.cur.fetchone() is None:
+            logging.info("Creating table APIKeys")
+            self.cur.execute("CREATE TABLE APIKeys(apiKey, status, lastChecked)")
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.con:
+            self.con.close()
+
+
+    def all_keys(self) -> list:
+        if self.cur is None:
+            raise ValueError("Cursor is not initialized")
+        self.cur.execute("SELECT apiKey FROM APIKeys WHERE status='yes'")
+        return self.cur.fetchall()
+
+    def deduplicate(self) -> None:
+        if self.con is None:
+            raise ValueError("Connection is not initialized")
+        if self.cur is None:
+            raise ValueError("Cursor is not initialized")
+        self.cur.execute("CREATE TABLE temp_table as SELECT apiKey, status, MAX(lastChecked) as lastChecked FROM APIKeys GROUP BY apiKey;")
+        self.cur.execute("DROP TABLE APIKeys;")
+        self.cur.execute("ALTER TABLE temp_table RENAME TO APIKeys;")
+        self.con.commit()
+
+    def delete(self, api_key: str) -> None:
+        if self.con is None:
+            raise ValueError("Connection is not initialized")
+        if self.cur is None:
+            raise ValueError("Cursor is not initialized")
+        self.cur.execute("DELETE FROM APIKeys WHERE apiKey=?", (api_key,))
+        self.con.commit()
+
+    def insert(self, api_key: str, status: str):
+        if self.con is None:
+            raise ValueError("Connection is not initialized")
+        if self.cur is None:
+            raise ValueError("Cursor is not initialized")
+        today = date.today()
+        self.cur.execute(
+            "INSERT INTO APIKeys(apiKey, status, lastChecked) VALUES(?, ?, ?)",
+            (api_key, status, today),
+        )
+        self.con.commit()
+
+    def key_exists(self, api_key: str) -> bool:
+        if self.cur is None:
+            raise ValueError("Cursor is not initialized")
+        self.cur.execute(
+            "SELECT apiKey FROM APIKeys WHERE apiKey=?", (api_key,))
+        return self.cur.fetchone() is not None
+
+    def __del__(self):
+        if self.con:
+            self.con.close()
